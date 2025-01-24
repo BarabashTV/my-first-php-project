c721d9827d4162269a7904bdae33b1faaf61f3c4
BarabashTV <164335010+BarabashTV@users.noreply.github.com>
2025-01-24 10:49:11 +0200

    first commit

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>My first PHP kotskasivu</title>
+    <link rel="stylesheet" href="./index.css" />
+  </head>
+  <body>
+    <h1>Heippa!</h1>
+<?php echo "My first PHP ECHO"; ?>
+
+<?php echo phpinfo(); ?>
+
+    <p>Minä</p>
+    <p>Tänään</p>
+  </body>
+</html>
